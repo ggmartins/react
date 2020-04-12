970114fefd2d15ef0c946668013eb495efa116bf
ggmartins <ggmartins@gmail.com>
2020-04-12 17:57:45 -0500

    feat: navbar v0.1

--- nextjs/webapp/pages/index.js
+++ nextjs/webapp/pages/index.js
@@ -1,1 +1,10 @@
-export default () => (<div>Hello Next.js</div>)
+import Navbar from "../components/navbar";
+
+const Index = () => (
+    <div>
+        <Navbar/>
+        <h1>Hello React Components v0.1</h1>
+    </div>
+);
+
+export default Index 
